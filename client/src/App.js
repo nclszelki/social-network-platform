e31f24dface140f97eb0e9744fe426a6eae68b51
Zelin Zhou <z273zhou@uwaterloo.caa>
2020-10-19 19:27:57 -0400

    Maintain login state

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import { AuthProvider } from './context/auth';
+import AuthRoute from './util/AuthRoute';
 
 import MenuBar from './components/MenuBar';
 import Home from './pages/Home';
@@ -13,14 +15,16 @@ import Register from './pages/Register';
 
 function App() {
   return (
-    <Router>
-      <Container>
-        <MenuBar />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-      </Container>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Container>
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 }
 
