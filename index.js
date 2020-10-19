73f1518e1ef2ba9927f2d4b8a3c6d82fd90e5a00
Zelin Zhou <z273zhou@uwaterloo.caa>
2020-10-19 00:07:26 -0400

    Adding auth checking middleware and create/ddelete post GraphQL mutations
    Adding create/delete post Mutation

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 const { ApolloServer } = require("apollo-server");
 const mongoose = require("mongoose");
 
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require("./config");
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: ({ req }) => ({ req }),
 });
 
 mongoose
   .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("MongoDB Connected");
-  })
-  .then(() => {
     return server.listen({ port: 5000 });
   })
   .then((res) => {
-    console.log(`server is running at ${res.url}`);
+    console.log(`Server running at ${res.url}`);
   });
