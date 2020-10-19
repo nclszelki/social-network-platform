fcf734f74e9aec61d78f2d26f45170322fb71320
Zelin Zhou <zzl@ZelindeMBP.lan>
2020-10-18 22:50:02 -0400

    Apollo Server setup/User Register Function

---
+++ index.js
@@ -0,0 +1,23 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require("./config");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .then(() => {
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`server is running at ${res.url}`);
+  });
