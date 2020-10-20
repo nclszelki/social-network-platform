04f548f2384ed5c658dc25c77d42eb0f2cf335b1
Zelin Zhou <z273zhou@uwaterloo.caa>
2020-10-19 20:54:01 -0400

    Publishing on Heroku/Netify

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,14 +1,15 @@
-import React, { useContext } from "react";
-import { Button, Card, Icon, Label, Image } from "semantic-ui-react";
-import { Link } from "react-router-dom";
-import moment from "moment";
+import React, { useContext } from 'react';
+import { Button, Card, Icon, Label, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import moment from 'moment';
 
-import { AuthContext } from "../context/auth";
-import LikeButton from "./LikeButton";
-import DeleteButton from "./DeleteButton";
+import { AuthContext } from '../context/auth';
+import LikeButton from './LikeButton';
+import DeleteButton from './DeleteButton';
+import MyPopup from '../util/MyPopup';
 
 function PostCard({
-  post: { body, createdAt, id, username, likeCount, commentCount, likes },
+  post: { body, createdAt, id, username, likeCount, commentCount, likes }
 }) {
   const { user } = useContext(AuthContext);
 
@@ -28,14 +29,16 @@ function PostCard({
       </Card.Content>
       <Card.Content extra>
         <LikeButton user={user} post={{ id, likes, likeCount }} />
-        <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
-          <Button color="blue" basic>
-            <Icon name="comments" />
+        <MyPopup content="Comment on post">
+          <Button labelPosition="right" as={Link} to={`/posts/${id}`}>
+            <Button color="blue" basic>
+              <Icon name="comments" />
+            </Button>
+            <Label basic color="blue" pointing="left">
+              {commentCount}
+            </Label>
           </Button>
-          <Label basic color="blue" pointing="left">
-            {commentCount}
-          </Label>
-        </Button>
+        </MyPopup>
         {user && user.username === username && <DeleteButton postId={id} />}
       </Card.Content>
     </Card>
